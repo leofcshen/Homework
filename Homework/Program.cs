e1b593fbead9897e5113d39dd8abd2a339e09c53
rovingwind93@gmail.com <rovingwind93@gmail.com>
2020-12-08 12:18:54 +0800

    update POS

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -19,6 +19,7 @@ namespace Homework
             Application.Run(new Form_Index());
             //Application.Run(new Form01_Hello());
             //Application.Run(new Form02_Loan());
+            faweuoghijawephg
             //Application.Run(new Form02_Loan_Report());
             //Application.Run(new Form03_POS());
             //Application.Run(new Form04_Grade());
