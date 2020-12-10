41098f8c276cecc8668bceaa9581eefcf4a65f3a
rovingwind93@gmail.com <rovingwind93@gmail.com>
2020-12-10 17:41:33 +0800

    123

--- Homework/FormT06.cs
+++ Homework/FormT06.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -53,6 +54,12 @@ namespace Homework
             return b;
         }
 
+        static string[] XY(string s) // 方法：回傳按下的按鈕 X , Y 座標
+        {
+            string[] sArray = s.Substring(s.IndexOf("[") + 1).TrimEnd(']').Split(','); // x,y
+            return sArray;
+        }
+
         private void button1_Click(object sender, EventArgs e) // 按鈕：要比對 %6 餘數
         {
             panel1.Controls.Clear();
@@ -70,45 +77,14 @@ namespace Homework
                     buttons[i, j].Name = $"btn[{i},{j}]";
                     buttons[i, j].Location = new Point(200 * i, 200 * j);
                     buttons[i, j].BackColor = Color.Beige;
-                    buttons[i, j].Text = arr[i, j].ToString();
-                    //if (int.Parse(buttons[i, j].Text) %6 == 1)
-                    //{
-                    //    buttons[i, j].BackgroundImage = bkg1;
-                    //    buttons[i, j].BackgroundImageLayout = ImageLayout.Zoom;
-                    //}
-                    //if (int.Parse(buttons[i, j].Text) % 6 == 2)
-                    //{
-                    //    buttons[i, j].BackgroundImage = bkg2;
-                    //    buttons[i, j].BackgroundImageLayout = ImageLayout.Zoom;
-                    //}
-                    //if (int.Parse(buttons[i, j].Text) % 6 == 3)
-                    //{
-                    //    buttons[i, j].BackgroundImage = bkg3;
-                    //    buttons[i, j].BackgroundImageLayout = ImageLayout.Zoom;
-                    //}
-                    //if (int.Parse(buttons[i, j].Text) % 6 == 4)
-                    //{
-                    //    buttons[i, j].BackgroundImage = bkg4;
-                    //    buttons[i, j].BackgroundImageLayout = ImageLayout.Zoom;
-                    //}
-                    //if (int.Parse(buttons[i, j].Text) % 6 == 5)
-                    //{
-                    //    buttons[i, j].BackgroundImage = bkg5;
-                    //    buttons[i, j].BackgroundImageLayout = ImageLayout.Zoom;
-                    //}
-                    //if (int.Parse(buttons[i, j].Text) % 6 == 0)
-                    //{
-                    //    buttons[i, j].BackgroundImage = bkg6;
-                    //    buttons[i, j].BackgroundImageLayout = ImageLayout.Zoom;
-                    //}
+                    buttons[i, j].Text = arr[i, j].ToString();                    
 
-                    panel1.Controls.Add(buttons[i, j]);
-
-                    foreach (Button control in panel1.Controls)
-                    {
-                        control.Click += Button_Click;                        
-                    }
+                    panel1.Controls.Add(buttons[i, j]);                    
                 }
+            }
+            foreach (Button control in panel1.Controls)
+            {
+                control.Click += Button_Click;
             }
         }
 
@@ -117,8 +93,6 @@ namespace Homework
             panel1.Controls.Clear();
             int[] a = { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6 };
             int[,] arr = randomList(a);
-
-            
 
             for (int i = 0; i < 6; i++)
             {
@@ -134,58 +108,101 @@ namespace Homework
                     buttons[i, j].Text = arr[i, j].ToString();
                     
                     panel1.Controls.Add(buttons[i, j]);
+                }                
+            }
+            foreach (Button control in panel1.Controls)
+            {
+                control.Click += Button_Click;
+            }
+        }
 
+        int flip = 0; // 翻牌次數
+        string strName1 = "";
+        string strText1 = "";
+        string strName2 = "";
+        string strText2 = "";
+        int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (int.Parse(((Button)sender).Text) % 6 == 1)
+            {
+                ((Button)sender).BackgroundImage = bkg1;
+                ((Button)sender).BackgroundImageLayout = ImageLayout.Zoom;
+                flip++;
+            }
+            else if (int.Parse(((Button)sender).Text) % 6 == 2)
+            {
+                ((Button)sender).BackgroundImage = bkg2;
+                ((Button)sender).BackgroundImageLayout = ImageLayout.Zoom;
+                flip++;
+            }
+            else if (int.Parse(((Button)sender).Text) % 6 == 3)
+            {
+                ((Button)sender).BackgroundImage = bkg3;
+                ((Button)sender).BackgroundImageLayout = ImageLayout.Zoom;
+                flip++;
+            }
+            else if (int.Parse(((Button)sender).Text) % 6 == 4)
+            {
+                ((Button)sender).BackgroundImage = bkg4;
+                ((Button)sender).BackgroundImageLayout = ImageLayout.Zoom;
+                flip++;
+            }
+            else if (int.Parse(((Button)sender).Text) % 6 == 5)
+            {
+                ((Button)sender).BackgroundImage = bkg5;
+                ((Button)sender).BackgroundImageLayout = ImageLayout.Zoom;
+                flip++;
+            }
+            else if (int.Parse(((Button)sender).Text) % 6 == 0)
+            {
+                ((Button)sender).BackgroundImage = bkg6;
+                ((Button)sender).BackgroundImageLayout = ImageLayout.Zoom;
+                flip++;
+            }
+            
+            if (flip == 1)
+            {
+                strName1 = ((Button)sender).Name;
+                strText1 = ((Button)sender).Text;
+                string[] sArray = XY(((Button)sender).Name);
+                x1 = int.Parse(sArray[0]);
+                y1 = int.Parse(sArray[1]);
+            }
+            else if (flip == 2)
+            {
+                strName2 = ((Button)sender).Name;
+                strText2 = ((Button)sender).Text;
+                string[] sArray = XY(((Button)sender).Name);
+                x2 = int.Parse(sArray[0]);
+                y2 = int.Parse(sArray[1]);
+
+                if (int.Parse(strText1)%6 == int.Parse(strText2) %  6) // 如果圖片一樣
+                {
                     foreach (Button control in panel1.Controls)
                     {
-                        control.Click += Button_Click;
+                        if ((control.Name == $"btn[{x1},{y1}]") || (control.Name == $"btn[{x2},{y2}]"))
+                        {
+                            control.Enabled = false;
+                        }
                     }
+                    flip = 0;
+                }
+                else if (int.Parse(strText1) % 6 != int.Parse(strText2) % 6)
+                {
+                    Thread.Sleep(1000);
+                    foreach (Button control in panel1.Controls)
+                    {
+                        if ((control.Name == $"btn[{x1},{y1}]") || (control.Name == $"btn[{x2},{y2}]"))
+                        {
+                            control.BackgroundImage = default;
+                        }
+                    }
+                    flip = 0;
                 }
             }
         }
-        int flip = 2;
-        private void Button_Click(object sender, EventArgs e)
-        {            
-            //if (flip > 0)
-            {
-                if  (int.Parse(((Button)sender).Text) % 6 == 1)
-                {
-                    ((Button)sender).BackgroundImage = bkg1;
-                    ((Button)sender).BackgroundImageLayout = ImageLayout.Zoom;
-                    flip--;
-                }
-                else if (int.Parse(((Button)sender).Text) % 6 == 2)
-                {
-                    ((Button)sender).BackgroundImage = bkg2;
-                    ((Button)sender).BackgroundImageLayout = ImageLayout.Zoom;
-                    flip--;
-                }
-                else if (int.Parse(((Button)sender).Text) % 6 == 3)
-                {
-                    ((Button)sender).BackgroundImage = bkg3;
-                    ((Button)sender).BackgroundImageLayout = ImageLayout.Zoom;
-                    flip--;
-                }
-                else if (int.Parse(((Button)sender).Text) % 6 == 4)
-                {
-                    ((Button)sender).BackgroundImage = bkg4;
-                    ((Button)sender).BackgroundImageLayout = ImageLayout.Zoom;
-                    flip--;
-                }
-                else if (int.Parse(((Button)sender).Text) % 6 == 5)
-                {
-                    ((Button)sender).BackgroundImage = bkg5;
-                    ((Button)sender).BackgroundImageLayout = ImageLayout.Zoom;
-                    flip--;
-                }
-                else if (int.Parse(((Button)sender).Text) % 6 == 0)
-                {
-                    ((Button)sender).BackgroundImage = bkg6;
-                    ((Button)sender).BackgroundImageLayout = ImageLayout.Zoom;
-                    flip--;
-                }                
-            }            
-        }
-
         // 為了方便觀看，我把值塞在 button.text，但 text 不能隱藏，如果button設定圖片做比對，建議把值塞在button.tag，用tag的值去比對。
         // 
     }
--- Homework/Program.cs
+++ Homework/Program.cs
@@ -16,7 +16,7 @@ namespace Homework
         {            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form_Index());
+            //Application.Run(new Form_Index());
             //Application.Run(new Form01_Hello());
             //Application.Run(new Form02_Loan());            
             //Application.Run(new Form02_Loan_Report());
@@ -34,7 +34,7 @@ namespace Homework
             //Application.Run(new Form15_GuessNumber());
             //Application.Run(new Form16_Alarm());
             //Application.Run(new FormT05_MineSweeper());
-            //Application.Run(new FormT06());
+            Application.Run(new FormT06());
         }
     }
 }
